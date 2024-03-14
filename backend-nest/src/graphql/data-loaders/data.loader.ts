72418a4caa827e4d497cb718a52ae38dab1340d4
EivydasV <eivydasvickus@gmail.com>
2024-03-15 00:12:25 +0200

    add watch to compose

--- backend-nest/src/graphql/data-loaders/data.loader.ts
+++ backend-nest/src/graphql/data-loaders/data.loader.ts
@@ -1,32 +1,32 @@
-import { Injectable, Scope } from '@nestjs/common';
-import { Repository, In } from 'typeorm';
-import DataLoader from 'dataloader';
-
-@Injectable({ scope: Scope.REQUEST })
-export class DataLoaderFactory {
-  constructor() {}
-
-  createLoader<T>(
-    repository: Repository<T>,
-    relation: string,
-  ): DataLoader<number, T[]> {
-    return new DataLoader<number, T[]>((keys) =>
-      this.batchLoadFn(keys, repository, relation),
-    );
-  }
-
-  private async batchLoadFn<T>(
-    ids: readonly number[],
-    repository: Repository<T>,
-    relation: string,
-  ): Promise<T[][]> {
-    const entitiesWithRelations = await repository.find({
-      select: ['id'],
-      where: {
-        id: In(ids as number[]),
-      },
-    });
-
-    return entitiesWithRelations.map((entity) => (entity as any)[relation]);
-  }
-}
+// import { Injectable, Scope } from '@nestjs/common';
+// import { Repository, In } from 'typeorm';
+// import DataLoader from 'dataloader';
+//
+// @Injectable({ scope: Scope.REQUEST })
+// export class DataLoaderFactory {
+//   constructor() {}
+//
+//   createLoader<T>(
+//     repository: Repository<T>,
+//     relation: string,
+//   ): DataLoader<number, T[]> {
+//     return new DataLoader<number, T[]>((keys) =>
+//       this.batchLoadFn(keys, repository, relation),
+//     );
+//   }
+//
+//   private async batchLoadFn<T>(
+//     ids: readonly number[],
+//     repository: Repository<T>,
+//     relation: string,
+//   ): Promise<T[][]> {
+//     const entitiesWithRelations = await repository.find({
+//       select: ['id'],
+//       where: {
+//         id: In(ids as number[]),
+//       },
+//     });
+//
+//     return entitiesWithRelations.map((entity) => (entity as any)[relation]);
+//   }
+// }
